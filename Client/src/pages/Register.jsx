3fabf11b13f39111a1052b14befd92599ebe7652
Srikanth parikibanda <parikibandasrikanthcse@gmail.com>
2026-08-19 15:27:21 +0530

    Remove success message and redirect after registration
    Removed success message display and redirect logic after registration.

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -234,12 +234,6 @@ function Register() {
         );
       }
 
-      setShowSuccess(true);
-
-      setTimeout(() => {
-  window.location.href = "https://www.ecell.in/eureka/register";
-}, 2000);
-
       // ==========================================
       // 4. RESET FORM
       // ==========================================
